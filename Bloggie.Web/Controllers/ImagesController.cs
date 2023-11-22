dc4f3ee110e1c22925beb330a24378ccfd71e76d
Venera Gxhufi <venera.goxhufi12@gmail.com>
2023-11-22 02:38:19 +0100

    Krijimi i Controllerit API per Images

---
+++ Bloggie.Web/Controllers/ImagesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using test1.Repositories;
+
+namespace test1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IImageRepository imageRepository;
+
+        //ctor
+        public ImagesController(IImageRepository imageRepository)
+        {
+            this.imageRepository = imageRepository;
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> UploadAsync(IFormFile file)
+        {
+            //call a repository
+            var imageURL = await imageRepository.UploadAsync(file);
+            if (imageURL == null)
+            {
+                return Problem("Something went wrong!", null, (int)HttpStatusCode.InternalServerError);
+            }
+            return new JsonResult(new { link = imageURL });
+
+        }
+    }
+
+
+}
+
