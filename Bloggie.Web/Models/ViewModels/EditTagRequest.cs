7cd36aa21e7313fb80ea39b22f3d1d152799be37
anda <ap56170@ubt-uni.net>
2023-11-22 01:59:51 -0800

    Rregullimi i Navbar

--- Bloggie.Web/Models/ViewModels/EditTagRequest.cs
+++ Bloggie.Web/Models/ViewModels/EditTagRequest.cs
@@ -2,5 +2,10 @@
 {
     public class EditTagRequest
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+
     }
 }
+
