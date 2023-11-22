28275ff725ef4368bbccc858ba1b597fb4fb39f8
anda <ap56170@ubt-uni.net>
2023-11-22 01:31:49 -0800

    Krijimi i folderit DATA

---
+++ Bloggie.Web/Data/BloggieDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Azure;
+using Bloggie.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Web.Data
+{
+    public class BloggieDbContext : DbContext
+    {
+        public BloggieDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+       
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
