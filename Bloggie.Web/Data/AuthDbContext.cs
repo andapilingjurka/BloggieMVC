17c74e697c8026ca62ada1d46d106831c17189ae
Venera Gxhufi <venera.goxhufi12@gmail.com>
2023-11-25 19:35:47 +0100

    Shtimi i rolit 'SuperAdmin' dhe 'User' ne listen e roleve ne klases AuthDbContext

--- Bloggie.Web/Data/AuthDbContext.cs
+++ Bloggie.Web/Data/AuthDbContext.cs
@@ -18,7 +18,9 @@ namespace Bloggie.Web.Data
             //Seed Roles(User,Admin,SuperAdmin)
 
             var adminRoleId = "5811453b-6a15-4bd0-82ca-fc98643dfb3b";
-           
+            var superAdminRoleId = "8518f0ce-cf65-4517-a2b9-2d8f50f1d5cd";
+            var userRoleId = "1a98fd2f-687a-4c4e-93da-b787a8c0ff05";
+
 
             var roles = new List<IdentityRole>
             {
@@ -29,5 +31,22 @@ namespace Bloggie.Web.Data
                     Id=adminRoleId,
                     ConcurrencyStamp= adminRoleId
                 },
+                new IdentityRole
+                {
+                    Name="SuperAdmin",
+                    NormalizedName="SuperAdmin",
+                    Id=superAdminRoleId,
+                    ConcurrencyStamp=superAdminRoleId
+                },
+                new IdentityRole
+                {
+                    Name= "User",
+                    NormalizedName = "User",
+                    Id=userRoleId,
+                    ConcurrencyStamp=userRoleId
+
+                }
+            };
+        }
     }
 }
