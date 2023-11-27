cf5f09c8ad3a30b2b45fe6a1b3dba49410e6757b
Dea <dk61403@ubt-uni.net>
2023-11-27 02:31:07 +0100

    Modifikime ne Blogs Controller per tu shfaq Likes

--- Bloggie.Web/Controllers/BlogsController.cs
+++ Bloggie.Web/Controllers/BlogsController.cs
@@ -1,4 +1,6 @@
-﻿using Bloggie.Web.Repositories;
+﻿using Bloggie.Web.Models.ViewModels;
+using Bloggie.Web.Repositories;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bloggie.Web.Controllers
@@ -6,19 +8,81 @@ namespace Bloggie.Web.Controllers
     public class BlogsController : Controller
     {
         private readonly IBlogPostRepository blogPostRepository;
+        private readonly IBlogPostLikeRepository blogPostLikeRepository;
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly UserManager<IdentityUser> userManager;
+     
 
-        public BlogsController(IBlogPostRepository blogPostRepository)
+        public BlogsController(IBlogPostRepository blogPostRepository,
+            IBlogPostLikeRepository blogPostLikeRepository,
+            SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
         {
             this.blogPostRepository = blogPostRepository;
+            this.blogPostLikeRepository = blogPostLikeRepository;
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+           
         }
 
         [HttpGet]
         public async Task<IActionResult> Index(string urlHandle)
         {
+            var liked = false;
             var blogPost = await blogPostRepository.GetByUrlHandleAsync(urlHandle);
+            var blogDetailsViewModel = new BlogDetailsViewModel();
 
-            return View(blogPost);
+            if (blogPost != null)
+            {
+                var totalLikes = await blogPostLikeRepository.GetTotalLikes(blogPost.Id);
+
+                if (signInManager.IsSignedIn(User))
+                {
+                    //Get like for this blog for this user
+                    var likesForBlog = await blogPostLikeRepository.GetLikesForBlog(blogPost.Id);
+
+                    var userId = userManager.GetUserId(User);
+
+                    if (userId != null)
+                    {
+                        var likeFromUser = likesForBlog.FirstOrDefault(x => x.UserId == Guid.Parse(userId));
+                        liked = likeFromUser != null;
+                    }
+
+
+                }
+
+
+              
+
+               
+
+                blogDetailsViewModel = new BlogDetailsViewModel
+                {
+                    Id = blogPost.Id,
+                    Content = blogPost.Content,
+                    PageTitle = blogPost.PageTitle,
+                    Author = blogPost.Author,
+                    FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                    Heading = blogPost.Heading,
+                    PublishedDate = blogPost.PublishedDate,
+                    ShortDescription = blogPost.ShortDescription,
+                    UrlHandle = blogPost.UrlHandle,
+                    Visible = blogPost.Visible,
+                    Tag = blogPost.Tag,
+                    TotalLikes = totalLikes,
+                    Liked = liked,
+                   
+
+
+                };
+
+            }
+            return View(blogDetailsViewModel);
         }
+
+        
+
+        
     }
 }
 
