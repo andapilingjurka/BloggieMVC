34b8a015954070d6dfdb51a36aa176bc74c39ce3
anda <ap56170@ubt-uni.net>
2023-11-29 02:52:16 -0800

    Metoda Register Validimi

--- Bloggie.Web/Controllers/AccountController.cs
+++ Bloggie.Web/Controllers/AccountController.cs
@@ -26,25 +26,30 @@ namespace Bloggie.Web.Controllers
         [HttpPost]
         public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
         {
-            var identityUser = new IdentityUser
+            if (ModelState.IsValid)
             {
-                UserName = registerViewModel.Username,
-                Email = registerViewModel.Email
-            };
-            var identityResult = await userManager.CreateAsync(identityUser, registerViewModel.Password);
-
-            if (identityResult.Succeeded)
-            {
-                // assign this user the "User" role
-                var roleIdentityResult = await userManager.AddToRoleAsync(identityUser, "User");
-
-                if (roleIdentityResult.Succeeded)
+                var identityUser = new IdentityUser
                 {
-                    // Show success notification
-                    return RedirectToAction("Register");
-                }
+                    UserName = registerViewModel.Username,
+                    Email = registerViewModel.Email
+                };
 
+                var identityResult = await userManager.CreateAsync(identityUser, registerViewModel.Password);
+
+                if (identityResult.Succeeded)
+                {
+                    //asign this user the "User" role
+                    var roleIdentityResult = await userManager.AddToRoleAsync(identityUser, "User");
+
+                    if (roleIdentityResult.Succeeded)
+                    {
+                        //Show success notification
+                        return RedirectToAction("Register");
+                    }
+                }
             }
+
+
             // Show error notification
             return View("Register");
 
