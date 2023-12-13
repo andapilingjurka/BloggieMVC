3e73f8b1604feea74ad2fe6d4bf9c5b17279953a
Dea <dk61403@ubt-uni.net>
2023-12-14 00:46:01 +0100

    Krijimi i Repositories per Feedback

---
+++ Bloggie.Web/Repositories/FeedbackRepository.cs
@@ -0,0 +1,47 @@
+﻿using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Web.Repositories
+{
+    public class FeedbackRepository:IFeedbackRepository
+
+    {
+        private readonly BloggieDbContext bloggieDbContext;
+
+        public FeedbackRepository(BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
+        public async Task<Feedback> AddAsync(Feedback feedback)
+        {
+            await bloggieDbContext.AddAsync(feedback);
+            await bloggieDbContext.SaveChangesAsync();
+            return feedback;
+
+        }
+
+        public async Task<Feedback?> DeleteAsync(int id)
+        {
+            var existingTag = await bloggieDbContext.Feedbacks.FindAsync(id);
+
+            if (existingTag != null)
+            {
+                bloggieDbContext.Feedbacks.Remove(existingTag); //kjo ska version async
+                await bloggieDbContext.SaveChangesAsync();
+
+                //Show success notification
+
+                return existingTag;
+
+            }
+            return null;
+        }
+
+        public async Task<IEnumerable<Feedback>> GetAllAsync()
+        {
+            return await bloggieDbContext.Feedbacks.ToListAsync();
+        }
+
+    }
+}
---
+++ Bloggie.Web/Repositories/IFeedbackRepository.cs
@@ -0,0 +1,13 @@
+﻿using Bloggie.Web.Models.Domain;
+
+namespace Bloggie.Web.Repositories
+{
+    public interface IFeedbackRepository
+    {
+        Task<IEnumerable<Feedback>> GetAllAsync();
+
+        Task<Feedback> AddAsync(Feedback feedback);
+
+        Task<Feedback?> DeleteAsync(int id);
+    }
+}
