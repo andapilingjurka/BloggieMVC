7d34af85d1abd731b079d9691e31fd67601c7593
Dea <dk61403@ubt-uni.net>
2023-12-14 00:41:01 +0100

    ViewModel per Feedback Request

---
+++ Bloggie.Web/Models/ViewModels/AddFeedbackRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Bloggie.Web.Models.ViewModels
+{
+    public class AddFeedbackRequest
+    {
+        public string Mesazhi { get; set; }
+        public DateTime DateAdded { get; set; }
+
+        public Guid UserId { get; set; }
+        public string Username { get; set; }
+    }
+}
